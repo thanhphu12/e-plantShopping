5296b48a70882a98b4135baf49eea6ab1b2c00a9
Tran Thanh Phu <92839288+thanhphu12@users.noreply.github.com>
2026-05-20 17:25:08 +0700

    Update views.py

--- onlinecourse-main/onlinecourse-main/onlinecourse/views.py
+++ onlinecourse-main/onlinecourse-main/onlinecourse/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Course, Submission
-
+from .models import Course, Submission, Choice
 
 @login_required
 def show_exam_result(request, course_id):
